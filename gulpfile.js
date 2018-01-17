937b14492e8557e5e77b79dacf0c617e5392dc8a
Simon Huang <simon.huang@github.com>
2018-01-17 11:54:20 +0100

    chore: modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
+let gulp = require('gulp');
+let less = require('gulp-less');
+let browserSync = require('browser-sync').create();
+let LessPluginAutoPrefix = require('less-plugin-autoprefix');
+let autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
+let cssmin = require('gulp-cssmin');
+let rename = require('gulp-rename');
 
 gulp.task('less', function() {
     return gulp.src('app/less/**/*.less') // Gets all files ending with .scss in app/scss
-      .pipe(less())
+      .pipe(less({
+          plugins: [autoprefixPlugin]
+      }))
       .pipe(gulp.dest('app/css'))
       .pipe(browserSync.reload({
-        stream: true
-      }))
+        stream: true}))
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('dist/css'));
+      
+      
   });
 
   gulp.task('watch', ['browserSync'], function (){
     gulp.watch('app/less/**/*.less', ['less']); 
+    gulp.watch('app/*.html', browserSync.reload); 
     // Other watchers
   })
 
