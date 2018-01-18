4f9354915b69832034bd396160b5391c77b7e9f2
Simon Huang <simon.huang@github.com>
2018-01-18 12:04:47 +0100

    chore: add plumber to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,12 @@ let LessPluginAutoPrefix = require('less-plugin-autoprefix');
 let autoprefixPlugin = new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
 let cssmin = require('gulp-cssmin');
 let rename = require('gulp-rename');
+let plumber = require('gulp-plumber');
 
 gulp.task('less', function() {
-    return gulp.src('app/less/**/*.less') // Gets all files ending with .scss in app/scss
-      .pipe(less({
+    return gulp.src('app/less/styles.less') // Gets all files ending with .less in app/scss
+    .pipe(plumber())  
+    .pipe(less({
           plugins: [autoprefixPlugin]
       }))
       .pipe(gulp.dest('app/css'))
@@ -19,11 +21,11 @@ gulp.task('less', function() {
       .pipe(gulp.dest('dist/css'));
   });
 
-  gulp.task('watch', ['browserSync'], function (){
+gulp.task('watch', ['browserSync'], function (){
     gulp.watch('app/less/**/*.less', ['less']); 
     gulp.watch('app/*.html', browserSync.reload); 
     // Other watchers
-  })
+    })
 
 gulp.task('browserSync', function() {
     browserSync.init({
