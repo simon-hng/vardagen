bd0f9b9a95ff2cb39b9ce559b31f01d80279b9ee
Simon Huang <simon.huang@github.com>
2018-01-23 16:09:04 +0100

    feat: changes scrollbehavior

--- app/js/scroll.js
+++ app/js/scroll.js
@@ -1,17 +1,26 @@
-var menu__items = document.querySelectorAll(".menu__item");
+var menu__link = document.querySelectorAll(".menu__item");
+var header = document.querySelector(".header");
 
-menu__items.forEach(function (item, index){
+menu__link.forEach(function (item, index){
     item.addEventListener('click', function(a) {
         a.preventDefault();
-
-        //gets href of target
-        var target = item.getAttribute("href");
-
-        //scrolling
-        document.querySelector(target).scrollIntoView({ 
-            behavior: 'smooth' 
-        });
         
+        //gets headerheight
+        var headerHeight = header.offsetHeight;
+        console.log(headerHeight);
+
+        //gets targets Y-position and
+        var target = item.getAttribute("href");
+        var targetOffset = document.querySelector(target).getBoundingClientRect();
+        var targetY = targetOffset.top;
+        console.log(targetY);
+
+        //scroll
+        window.scrollBy({
+            top: targetY - headerHeight,
+            behavior: 'smooth',
+        });
+
         //hides menu on mobile
         document.querySelector('.menu-checkbox').checked = false;
     })
