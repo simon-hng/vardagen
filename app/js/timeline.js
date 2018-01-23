7402881283a2a0cf86214e67dc0834e8edc42a3b
Simon Huang <simon.huang@github.com>
2018-01-23 17:40:13 +0100

    refactor: reworked timeline script

--- app/js/timeline.js
+++ app/js/timeline.js
@@ -1,102 +1,19 @@
-var items_collection = document.querySelectorAll(".event__item");
-var texts_collection = document.querySelectorAll(".timeline__text");
-var select = "event__item--selected"; //Class to highlight selected
-var hide = "visually-hidden"; //Class to hide visually
+const eventItems = document.querySelectorAll(".js-event-item");
+const timelineItems = document.querySelectorAll(".js-timeline-content");
 
+const isSelected = "is-selected";
+const isVisible = "is-shown";
 
-//deselect all items
-var deselect = function(){
-    items_collection.forEach(function (item){
-        item.classList.remove(select);
-    })
-    texts_collection.forEach(function (item){
-        item.classList.add(hide);
-    })
+function toggleClass(elements, index, className) {
+   elements.forEach((element) => {
+      element.classList.remove(className);
+   });
+   elements[index].classList.add(className);
 }
 
-//Start with event__00 and text__00
-deselect();
-items_collection[0].classList.add(select);
-texts_collection[0].classList.remove(hide);
-
-//toggles classes on click
-items_collection.forEach(function (item, index){
-    item.addEventListener('click', function(){
-        deselect();
-        item.classList.add(select);
-        texts_collection[index].classList.remove(hide);
-    })
-})
-
-
-/* //timeline events
-var event__item = document.getElementsByClassName("event__item");
-
-var item__0 = event__item.item(0);
-var item__1 = event__item.item(1);
-var item__2 = event__item.item(2);
-var item__3 = event__item.item(3);
-
-//timeline text
-var timeline = document.getElementsByClassName("timeline__text");
-var text__0 = timeline.item(0);
-var text__1 = timeline.item(1);
-var text__2 = timeline.item(2);
-var text__3 = timeline.item(3);
-
-
-//deselect all events/timeline-texts
-var deselect = function(){
-    item__0.classList.remove("event__item--selected");
-    item__1.classList.remove("event__item--selected");
-    item__2.classList.remove("event__item--selected");
-    item__3.classList.remove("event__item--selected");
-
-    text__0.classList.add("visually-hidden");
-    text__1.classList.add("visually-hidden");
-    text__2.classList.add("visually-hidden");
-    text__3.classList.add("visually-hidden");
-};
-
-//starts with item__0 selected
-deselect();
-item__0.classList.add("event__item--selected");
-text__0.classList.remove("visually-hidden");
-
-//TODO: repetitive
-item__0.onclick = function(){
-    deselect();
-    item__0.classList.add("event__item--selected");
-    text__0.classList.remove("visually-hidden");
-};
-
-item__1.onclick = function(){
-    deselect();
-    item__1.classList.add("event__item--selected");
-    text__1.classList.remove("visually-hidden");
-};
-item__2.onclick = function(){
-    deselect();
-    item__2.classList.add("event__item--selected");
-    text__2.classList.remove("visually-hidden");
-};
-item__3.onclick = function(){
-    deselect();
-    item__3.classList.add("event__item--selected");
-    text__3.classList.remove("visually-hidden");
-};
-
-
-
-var event_item = document.getElementsByClassName("event__item");
-var timeline = document.getElementsByClassName("timeline__text");
-var item_count = event_item.length;
-var n = 0;
-
-
-//reset
-while(n < item_count){
-    text = timeline.item(n);
-    text.classList.add("visually-hidden");
-    n = n + 1;
-}; */ 
+eventItems.forEach((eventItem, index) => {
+   eventItem.addEventListener('click', () => {
+      toggleClass(eventItems, index, isSelected);
+      toggleClass(timelineItems, index, isVisible);
+   });
+});
