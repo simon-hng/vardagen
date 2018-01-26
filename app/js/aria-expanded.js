84cdb0f16d2d1195cf8ce1926f38d3a0280595a6
Simon Huang <simon.huang@github.com>
2018-01-26 10:41:31 +0100

    feat: improve accessibility

--- app/js/aria-expanded.js
+++ app/js/aria-expanded.js
@@ -1,16 +1,15 @@
 
-const toggler = document.querySelectorAll('.js-aria-toggle');
+const toggler = document.querySelectorAll('.js-aria-expanded-toggle');
 const el = document.querySelectorAll('[aria-expanded]');
 
 
 el.forEach(function(item){
     item.setAttribute('aria-expanded', 'false');
 });
-el.foreach
 
 toggler.forEach(function(item, index){
     item.addEventListener('click', () => {
-        if(el[index].getAttribute('aria-expanded') == 'false'){
+        if(el[index].getAttribute('aria-expanded') === 'false'){
             el[index].setAttribute('aria-expanded', true);
         } else {
             el[index].setAttribute('aria-expanded', false);
